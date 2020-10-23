32e76158c084cccef7f94bc0eb566247d994f4ba
Paul Okello <paulokello273@gmail.com>
2020-10-23 11:00:36 +0300

    user model created

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+const { MONGODB } = require("./config.js");
+
 const typeDefs = gql`
   type Query {
     sayHi: String!
@@ -10,10 +13,20 @@ const resolvers = {
     sayHi: () => "Hello World",
   },
 };
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server running at ${res.url}`);
-});
+
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
