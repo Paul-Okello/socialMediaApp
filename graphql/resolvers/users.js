310e5b069fdcae568d4460917f86116d99fbcd0e
Paul Okello <paulokello273@gmail.com>
2020-10-23 12:08:31 +0300

    hash password and create an auth token

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,41 @@
 const User = require("../../models/User");
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../../config");
 module.exports = {
   Mutation: {
-    register(_, args, context, info) {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
       // Validate user data
       // Make sure user doesn't already exist
       //hash password and create an auth token
+      password = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+      const res = await newUser.save();
+
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };
