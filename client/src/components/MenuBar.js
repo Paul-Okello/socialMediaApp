ce9ae4c6683250684b8a4a3e52ad1ae38e165a7d
Paul Okello <paulokello273@gmail.com>
2020-10-26 17:33:48 +0300

    menubar component

---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
+
+export default function MenuBar() {
+  const [activeItem, setActiveItem] = useState("");
+
+  handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <Menu pointing secondary>
+      <Menu.Item
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+      />
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="login"
+          active={activeItem === "login"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="logout"
+          active={activeItem === "logout"}
+          onClick={handleItemClick}
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+}
