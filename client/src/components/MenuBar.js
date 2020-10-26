3e6d4267b0dca858aa3d6056fbe993ade05a9972
Paul Okello <paulokello273@gmail.com>
2020-10-26 17:50:14 +0300

    ux

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -3,12 +3,14 @@ import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 export default function MenuBar() {
-  const [activeItem, setActiveItem] = useState("");
+  const pathname = window.location.pathname;
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary size="massive" color="brown">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
