0eecb45a649367bd4f272d2b4185b5decc78569f
Paul Okello <paulokello273@gmail.com>
2020-11-09 22:52:56 +0300

    likebutton

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -1,25 +1,39 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function LikeButton({ post: { id, likeCount, likes } }) {
   const [liked, setLiked] = useState(false);
+
   useEffect(() => {
     if (user && likes.find((like) => like.username === user.username)) {
       setLiked(true);
-    } else {
-      setLiked(false);
-    }, [user, likes]
-  });
+    } else setLiked(false);
+  }, [user, likes]);
+
+  const likeButton = user ? (
+    liked ? (
+      <Button color="brown">
+        <Icon name="heart" />
+      </Button>
+    ) : (
+      <Button color="brown" basic>
+        <Icon name="heart" />
+      </Button>
+    )
+  ) : (
+    <Button as={Link} to="/login" color="brown" basic>
+      <Icon name="heart" />
+    </Button>
+  );
+
   return (
     <Button as="div" labelPosition="right" onClick={likePost}>
-        <Button color="brown" basic>
-        <Icon name="heart" />
-        Like
-        </Button>
-        <Label as="a" basic color="red" pointing="left">
+      {likeButton}
+      <Label as="a" basic color="red" pointing="left">
         {likeCount}
-        </Label>
+      </Label>
     </Button>
-  )
+  );
 }
 
 export default LikeButton;
