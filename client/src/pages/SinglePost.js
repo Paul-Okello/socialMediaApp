3289296a0e2f9f019b7429178c57bd8ba95eb353
Paul Okello <paulokello273@gmail.com>
2020-11-11 18:06:07 +0300

    singlepost component

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,8 +1,88 @@
-import React from "react";
+import React, { useContext } from "react";
 import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import {
+  Card,
+  Dimmer,
+  Grid,
+  Image,
+  Loader,
+  Button,
+  Label,
+  Icon,
+} from "semantic-ui-react";
+import moment from "moment";
+import { AuthContext } from "../Context/auth";
+import LikeButton from "../components/LikeButton";
 
 function SinglePost(props) {
   const postId = props.match.params.postId;
+  const { user } = useContext(AuthContext);
+  const {
+    data: { getPost },
+  } = useQuery(FETCH_POST_QUERY, {
+    variables: {
+      postId,
+    },
+  });
+
+  let postMarkup;
+  if (!getPost) {
+    postMarkup = (
+      <Dimmer active>
+        <Loader size="massive">Loading posts...</Loader>
+      </Dimmer>
+    );
+  } else {
+    const {
+      id,
+      body,
+      createdAt,
+      username,
+      comments,
+      likes,
+      likeCount,
+      commentCount,
+    } = getPost;
+
+    postMarkup = (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image
+              src="https://static.wikia.nocookie.net/avatar/images/e/e1/Zaheer.png/revision/latest/top-crop/width/360/height/360?cb=20140825190111"
+              size="small"
+            />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+              </Card.Content>
+              <hr />
+              <Card.Content>
+                <LikeButton user={user} post={{ id, likeCount, likes }} />
+                <Button
+                  as="div"
+                  labelPosition="right"
+                  onClick={() => console.log("Comment on post!")}
+                >
+                  <Button basic color="blue">
+                    <Icon name="comments" />
+                  </Button>
+                  <Label basic color="blue" pointing="left">
+                    {commentCount}
+                  </Label>
+                </Button>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
   return <div></div>;
 }
 const FETCH_POST_QUERY = gql`
