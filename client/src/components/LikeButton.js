8f8c30fb17f94621555ca0af87c0f784d81bc62b
Paul Okello <paulokello273@gmail.com>
2020-11-11 17:25:31 +0300

    like button complete

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Button, Label, Icon } from "semantic-ui-react";
 
-function LikeButton({ post: { id, likeCount, likes } }) {
+function LikeButton({ user, post: { id, likeCount, likes } }) {
   const [liked, setLiked] = useState(false);
 
   useEffect(() => {
@@ -9,6 +12,10 @@ function LikeButton({ post: { id, likeCount, likes } }) {
       setLiked(true);
     } else setLiked(false);
   }, [user, likes]);
+
+  const [likePost] = useMutation(LIKE_POST_MUTATION, {
+    variables: { postId: id },
+  });
 
   const likeButton = user ? (
     liked ? (
@@ -35,5 +42,17 @@ function LikeButton({ post: { id, likeCount, likes } }) {
     </Button>
   );
 }
+const LIKE_POST_MUTATION = gql`
+  mutation likePost($postId: ID!) {
+    likePost(postId: $postId) {
+      id
+      likes {
+        id
+        username
+      }
+      likeCount
+    }
+  }
+`;
 
 export default LikeButton;
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { MONGODB } = require("./config.js");
 const typeDefs = require("./graphql/typeDefs");
 
 const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -12,14 +15,14 @@ const server = new ApolloServer({
 });
 
 mongoose
-  .connect(MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("MongoDB Connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
   });
